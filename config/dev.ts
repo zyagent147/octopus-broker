ffbe38de3ea4858b688c143343b85a4860c36fd0
AlbertinWuhan <4461183492961016-AlbertinWuhan@noreply.coze.cn>
2026-04-08 01:35:57 +0800

    config: 配置小程序开发环境连接本地后端

--- config/dev.ts
+++ config/dev.ts
@@ -4,6 +4,10 @@ export default {
   
   mini: {
     debugReact: true,
+    // 开发环境使用本地后端
+    defineConstants: {
+      PROJECT_DOMAIN: JSON.stringify('http://localhost:3000'),
+    },
   },
   h5: {}
 } satisfies UserConfigExport<'vite'>
